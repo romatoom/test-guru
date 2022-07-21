7c67e5e42079e71df6fdfb78ed575078ab49a979
Roman Ivanov <romani@epayservices.com>
2022-07-21 18:21:13 +0500

    add: additional associations has been added in models test, user + refactor methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,13 +2,12 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :users_tests
+  has_many :users, through: :users_tests
 
   class << self
     def tests_by_category_desc(category_title)
-      Test.joins(:category)
-        .where({ category: { title: category_title } })
-        .order(title: :desc)
-        .pluck(:title)
+      category = Category.find_by(title: category_title)
+      category.nil? ? [] : category.tests.order(title: :desc).pluck(:title)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :user_tests
+  has_many :users_tests
+  has_many :tests, through: :users_tests
 
   def tests_by_level(level)
-    Test.joins(:users_tests).where(users_tests: { user_id: id }, level: level)
+    tests.where(level: level)
   end
 end
