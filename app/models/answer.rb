c5520d66e1882b4ad472f9aca1c5538807f452ad
Roman Ivanov <romani@epayservices.com>
2022-07-24 19:19:43 +0500

    refactor: validate answers count

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,22 +5,14 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
-  validate :validate_count_on_create, on: :create
-  validate :validate_count_on_update, on: :update
-
-  scope :count_with_question_id, ->(question_id) { where(question_id: question_id).count }
+  validate :validate_answers_count
 
   private
 
-  def validate_count_on_create
-    if Answer.count_with_question_id(question_id) >= 4
-      errors.add(:question_id, "You cannot create more than 4 answers for question")
-    end
-  end
-
-  def validate_count_on_update
-    unless (1..4).include?(Answer.count_with_question_id(question_id))
-      errors.add(:question_id, "The number of answers for question should be from 1 to 4")
+  def validate_answers_count
+    return if persisted?
+    if question.answers.count >= 4
+      errors.add(:base, "You cannot create more than 4 answers for question")
     end
   end
 end
