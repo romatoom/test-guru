02ae8ff4c308e2251c6012f5d6cebe0c3ddc274c
Roman Ivanov <romani@epayservices.com>
2022-08-05 17:35:28 +0500

    add: helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,15 @@
 module QuestionsHelper
+  def current_year
+    Date.today.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
+
+  def question_header(test)
+    return "Создание вопроса для теста \"#{test.title}\" с уровнем сложности #{test.level}" if action_name == "new"
+
+    "Редактирование вопроса для теста \"#{test.title}\" с уровнем сложности #{test.level}"
+  end
 end
