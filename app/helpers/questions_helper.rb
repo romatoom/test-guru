ac9ad0812752fe5192a1fdb062ae756b39dff19e
Roman Ivanov <romani@epayservices.com>
2022-08-08 10:31:03 +0500

    refactor

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,9 @@
 module QuestionsHelper
   def question_header(question)
-    return "Создание вопроса для теста \"#{question.test.title}\" с уровнем сложности #{question.test.level}" if question.new_record?
-
-    "Редактирование вопроса для теста \"#{question.test.title}\" с уровнем сложности #{question.test.level}"
+    if question.new_record?
+      "Создание вопроса для теста \"#{question.test.title}\" с уровнем сложности #{question.test.level}"
+    else
+      "Редактирование вопроса для теста \"#{question.test.title}\" с уровнем сложности #{question.test.level}"
+    end
   end
 end
