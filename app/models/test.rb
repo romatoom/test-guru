dd3731fd69adb9e819464eef79900b62a4fef042
Roman Ivanov <romani@epayservices.com>
2022-08-09 18:25:01 +0500

    update: models for tests and questions (add dependent: :destroy)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :users_tests
   has_many :users, through: :users_tests
   belongs_to :author, class_name: "User", foreign_key: "author_id"
