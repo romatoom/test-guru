f22beb7f1949d041b4be92a425f108fbcdcc6528
Roman Ivanov <romani@epayservices.com>
2022-08-12 21:20:28 +0500

    refactor: use has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
-  include Auth
-
   has_many :users_tests, dependent: :destroy
   has_many :tests, through: :users_tests
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 
   def tests_by_level(level)
     tests.where(level: level)
