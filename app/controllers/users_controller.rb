b39b3cffd0402664b6071c5903e1fd98b72d2a0f
Roman Ivanov <romani@epayservices.com>
2022-08-13 17:43:14 +0500

    add: user ID to the session after registering it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
     @user[:email] = @user[:email].downcase
 
     if @user.save
+      session[:user_id] = @user.id
+
       redirect_to tests_path
     else
       render :new
