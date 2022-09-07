6217d466e3265af87aacc267c468ff82c019cfc5
Roman Ivanov <romani@epayservices.com>
2022-09-07 20:03:35 +0500

    add: Rails Mailer

---
+++ app/mailers/tests_mailer.rb
@@ -0,0 +1,8 @@
+class TestsMailer < ApplicationMailer
+  def completed_test(user_test)
+    @user = user_test.user
+    @test = user_test.test
+
+    mail to: @user.email, subject: "Вы завершили тест TestGuru"
+  end
+end
