da53cc0bfb5b1d9ce19f2842b8ba160fa21b4d28
Roman Ivanov <romani@epayservices.com>
2022-09-08 12:08:48 +0500

    add: columns type, last_name, first_name into users table, rename name -> nickname, refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_050659) do
+ActiveRecord::Schema.define(version: 2022_09_08_065609) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_09_07_050659) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 2022_09_07_050659) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
