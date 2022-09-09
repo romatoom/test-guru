fab8d451090dd446ad2174aafb53618495eb5d4c
Roman Ivanov <romani@epayservices.com>
2022-09-09 12:00:34 +0500

    add: answers and questions move to admin namespace

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,5 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i(start)
-
-  rescue_from SQLite3::ConstraintException, with: :rescue_with_constraint_error
+  before_action :set_test, only: :start
 
   def index
     @tests = Test.all
@@ -13,23 +11,8 @@ class TestsController < ApplicationController
   end
 
   private
-  
+
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def rescue_with_constraint_error(e)
-    flash.now[:danger] = "Произошла ошибка: #{e.message}"
-
-    actions = {
-      "create" => :new,
-      "update" => :edit
-    }
-
-    if actions[action_name].nil?
-      render :index
-    else
-      render actions[action_name]
-    end
   end
 end
