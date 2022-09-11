fc4309528504cf05cfa85aa44ff702208f40490c
Roman Ivanov <romani@epayservices.com>
2022-09-11 12:37:23 +0500

    add: locales for test passage, locales for mail about tests complete

--- app/helpers/users_tests_helper.rb
+++ app/helpers/users_tests_helper.rb
@@ -1,9 +1,9 @@
 module UsersTestsHelper
   def paragraph_for_test_result(user_test)
     if user_test.success?
-      "<p class='test-result passed'>#{user_test.result_in_persent}% - вы прошли тест</p>".html_safe
+      "<p class='test-result passed'>#{user_test.result_in_persent}% - #{t(".passed")}</p>".html_safe
     else
-      "<p class='test-result failed'>#{user_test.result_in_persent}% - вы не прошли тест</p>".html_safe
+      "<p class='test-result failed'>#{user_test.result_in_persent}% - #{t(".failed")}</p>".html_safe
     end
   end
 end
