6c8f82336f366ac8dd2edafca29fd0610bffb691
Roman Ivanov <romani@epayservices.com>
2022-09-26 20:18:05 +0500

    fix: default_url_options of action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'nameless-savannah-52200.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'romatoom-test-guru.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
