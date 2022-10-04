80caadc78340679041157093e0ddce39986e7a81
Roman Ivanov <romani@epayservices.com>
2022-10-04 12:04:14 +0500

    refactor: category count in main page controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,6 @@ class MainController < ApplicationController
     available_tests = Test.available
     @tests_count = available_tests.length
 
-    categories_of_tests_available = available_tests.map { |t| t.category.title }
-    @categories_count = categories_of_tests_available.uniq.length
+    @categories_count = available_tests.distinct.count(:category_id)
   end
 end
